635cf54162d399292b16c85d7fb02687df18bbb9
yaejinkong <0910a@naver.com>
2024-11-06 14:29:37 +0900

    [build] withbeetravel 초기 세팅

---
+++ src/test/java/withbeetravel/WithbeetravelApplicationTests.java
@@ -0,0 +1,13 @@
+package withbeetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithbeetravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
