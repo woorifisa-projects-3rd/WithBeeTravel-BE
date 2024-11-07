5c96c84f0d472faabbc50e5a732d4be589e2bd7f
yaejinkong <0910a@naver.com>
2024-11-07 14:35:55 +0900

    [feat/TK-27]: SharedPayment 엔티티 추가
    - Category enum 추가

---
+++ src/main/java/withbeetravel/domain/Category.java
@@ -0,0 +1,12 @@
+package withbeetravel.domain;
+
+public enum Category {
+    TRANSPORTATION,  // 교통
+    FOOD,            // 식비
+    ACCOMMODATION,   // 숙박
+    TOUR,            // 관광
+    ACTIVITY,        // 액티비티
+    SHOPPING,        // 쇼핑
+    FLIGHT,          // 항공
+    ETC              // 기타
+}
---
+++ src/main/java/withbeetravel/domain/SharedPayment.java
@@ -0,0 +1,88 @@
+package withbeetravel.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "shared_payments")
+public class SharedPayment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shared_payment_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "travel_member_id", nullable = false)
+    private TravelMember travelMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "currency_id", nullable = false)
+    private Currency currency;
+
+    @Column(name = "payment_amount", nullable = false)
+    private int paymentAmount;
+
+    @Column(name = "foreign_payment_amount")
+    private double foreignPaymentAmount;
+
+    @Column(name = "exchange_rate")
+    private double exchangeRate;
+
+    @Column(name = "payment_comment")
+    private String paymentComment;
+
+    @Column(name = "payment_image")
+    private String profileImage;
+
+    @Column(name = "is_manually_added", nullable = false)
+    private int isManuallyAdded;
+
+    @Column(name = "is_all_members_participated", nullable = false)
+    private int isAllMembersParticipated;
+
+    @Column(name = "category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(name = "store_name", nullable = false)
+    private String storeName;
+
+    @Column(name = "payment_date", nullable = false)
+    private LocalDateTime paymentDate;
+
+    protected SharedPayment() {}
+
+    @Builder
+    public SharedPayment(Long id,
+                         TravelMember travelMember,
+                         Currency currency,
+                         int paymentAmount,
+                         double foreignPaymentAmount,
+                         double exchangeRate,
+                         String paymentComment,
+                         String profileImage,
+                         int isManuallyAdded,
+                         int isAllMembersParticipated,
+                         Category category,
+                         String storeName,
+                         LocalDateTime paymentDate) {
+        this.id = id;
+        this.travelMember = travelMember;
+        this.currency = currency;
+        this.paymentAmount = paymentAmount;
+        this.foreignPaymentAmount = foreignPaymentAmount;
+        this.exchangeRate = exchangeRate;
+        this.paymentComment = paymentComment;
+        this.profileImage = profileImage;
+        this.isManuallyAdded = isManuallyAdded;
+        this.isAllMembersParticipated = isAllMembersParticipated;
+        this.category = category;
+        this.storeName = storeName;
+        this.paymentDate = paymentDate;
+    }
+}
