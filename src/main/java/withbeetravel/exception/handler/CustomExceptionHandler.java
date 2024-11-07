118325387b11a6742c53e5e3bd79e0eb3e49f28e
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-11-07 22:08:41 +0900

    [feat]: Custom Exception 하나로 통합

--- src/main/java/withbeetravel/exception/handler/CustomExceptionHandler.java
+++ src/main/java/withbeetravel/exception/handler/CustomExceptionHandler.java
@@ -3,34 +3,14 @@ package withbeetravel.exception.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import withbeetravel.exception.*;
+import withbeetravel.exception.CustomException;
 import withbeetravel.exception.dto.ErrorResponseDto;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(AuthException.class)
-    protected ResponseEntity<ErrorResponseDto> handleAuthException(AuthException e) {
-        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
-    }
-
-    @ExceptionHandler(PaymentException.class)
-    protected ResponseEntity<ErrorResponseDto> handlePaymentException(PaymentException e) {
-        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
-    }
-
-    @ExceptionHandler(SettlementException.class)
-    protected ResponseEntity<ErrorResponseDto> handleSettlementException(SettlementException e) {
-        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
-    }
-
-    @ExceptionHandler(TravelException.class)
-    protected ResponseEntity<ErrorResponseDto> handleTravelException(TravelException e) {
-        return ErrorResponseDto.toResponseEntity(e.getErrorCode());
-    }
-
-    @ExceptionHandler(ValidationException.class)
-    protected ResponseEntity<ErrorResponseDto> handleValidationException(ValidationException e) {
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponseDto> handleAuthException(CustomException e) {
         return ErrorResponseDto.toResponseEntity(e.getErrorCode());
     }
 }
