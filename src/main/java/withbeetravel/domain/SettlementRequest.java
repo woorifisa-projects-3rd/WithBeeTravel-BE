06ea211506b3a36d48ce9d20ca279b10aad5cb0f
yaejinkong <0910a@naver.com>
2024-11-10 10:07:21 +0900

    [feat/TK-69]: SettlementRequest 엔티티에 disagreeCount(정산 미동의 인원수) 필드 추가

--- src/main/java/withbeetravel/domain/SettlementRequest.java
+++ src/main/java/withbeetravel/domain/SettlementRequest.java
@@ -28,10 +28,12 @@ public class SettlementRequest {
     @Column(name = "request_end_time")
     private LocalDateTime requestEndTime;
 
-
     @Column(name = "request_status", nullable = false)
     @Enumerated(EnumType.STRING)
     private RequestStatus requestStatus;
+
+    @Column(name = "disagree_count", nullable = false)
+    private int disagreeCount;
 
     protected SettlementRequest() {}
 
@@ -40,11 +42,13 @@ public class SettlementRequest {
                              Travel travel,
                              LocalDateTime requestStartTime,
                              LocalDateTime requestEndTime,
-                             RequestStatus requestStatus) {
+                             RequestStatus requestStatus,
+                             int disagreeCount) {
         this.id = id;
         this.travel = travel;
         this.requestStartTime = requestStartTime;
         this.requestEndTime = requestEndTime;
         this.requestStatus = requestStatus;
+        this.disagreeCount = disagreeCount;
     }
 }
