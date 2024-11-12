f4a34e331f594869e4a978a630c3e634c12c1b05
HoChoRoo <rttkhc0304@gmail.com>
2024-11-12 12:05:37 +0900

    [style]: 계좌번호 생성 및 유니크 확인 후 저장 기능 구현부분 주석

--- src/main/java/withbeetravel/service/banking/AccountServiceImpl.java
+++ src/main/java/withbeetravel/service/banking/AccountServiceImpl.java
@@ -23,12 +23,14 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final UserRepository userRepository;
 
+    // 계좌 조회
     public List<AccountResponse> showAll(Long userId) {
         List<Account> accounts = accountRepository.findByUserId(userId);
 
         return accounts.stream().map(AccountResponse::from).collect(Collectors.toList());
     }
 
+    // 계좌 내역 조회
     public Account showAccount(Long accountId) {
         return accountRepository.findById(16L).orElseThrow();
     }
@@ -50,7 +52,7 @@ public class AccountServiceImpl implements AccountService {
         return accountRepository.save(account);
     }
 
-    // 유니크 계좌번호 생성
+    // 유니크 계좌번호 확인
     public String generateUniqueAccountNumber() {
         String accountNumber;
 
@@ -62,6 +64,7 @@ public class AccountServiceImpl implements AccountService {
         return accountNumber;
     }
 
+    // 계좌번호 랜덤 생성
     public String generateAccountNumber() {
         Random random = new Random();
         StringBuilder accountNumber = new StringBuilder("1"); // 첫 번째 자리는 항상 1
@@ -74,6 +77,7 @@ public class AccountServiceImpl implements AccountService {
         return accountNumber.toString();
     }
 
+    // 계좌번호 존재 유무 확인
     public boolean isAccountNumberExists(String accountNumber) {
         Optional<Account> existingAccount = accountRepository.findByAccountNumber(accountNumber);
         return existingAccount.isPresent(); // 존재하면 true 반환
