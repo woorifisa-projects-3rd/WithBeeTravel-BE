35d7e37930a2ff73697a2835933bafc062300369
HoChoRoo <rttkhc0304@gmail.com>
2024-11-13 16:12:31 +0900

    [fix]: AccountResponse에 accountId 추가로 넘겨줄 수 있게 수정

--- src/main/java/withbeetravel/dto/banking/account/AccountResponse.java
+++ src/main/java/withbeetravel/dto/banking/account/AccountResponse.java
@@ -7,18 +7,20 @@ import withbeetravel.domain.Product;
 @Getter
 public class AccountResponse {
 
+    private Long accountId;
     private String accountNumber;
     private long balance;
     private Product product;
 
-    public AccountResponse(String accountNumber, long balance, Product product){
+    public AccountResponse(Long accountId,String accountNumber, long balance, Product product){
+        this.accountId = accountId;
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.product = product;
     }
 
     public static AccountResponse from(Account account){
-        return new AccountResponse(account.getAccountNumber(), account.getBalance(), account.getProduct());
+        return new AccountResponse(account.getId(), account.getAccountNumber(), account.getBalance(), account.getProduct());
     }
 
 }
