3d622271b3afb284182c7ff76875de4d9c9e39cc
yaejinkong <0910a@naver.com>
2024-11-14 22:22:34 +0900

    [feat/TK-128]: List<ShowOtherSettlementResponse> 생성 로직 구현
    - 자신 이외의 여행 멤버들의 이름과 개인 부담 금액을 보여주는 List

--- src/main/java/withbeetravel/dto/settlement/ShowOtherSettlementResponse.java
+++ src/main/java/withbeetravel/dto/settlement/ShowOtherSettlementResponse.java
@@ -2,10 +2,14 @@ package withbeetravel.dto.settlement;
 
 public class ShowOtherSettlementResponse {
     private String name;
-    private int actualBurdenCost;
+    private int totalPaymentCost;
 
-    public ShowOtherSettlementResponse(String name, int actualBurdenCost) {
+    private ShowOtherSettlementResponse(String name, int totalPaymentCost) {
         this.name = name;
-        this.actualBurdenCost = actualBurdenCost;
+        this.totalPaymentCost = totalPaymentCost;
+    }
+
+    public static ShowOtherSettlementResponse of (String name, int totalPaymentCost) {
+        return new ShowOtherSettlementResponse(name, totalPaymentCost);
     }
 }
--- src/main/java/withbeetravel/service/settlement/SettlementServiceImpl.java
+++ src/main/java/withbeetravel/service/settlement/SettlementServiceImpl.java
@@ -3,29 +3,72 @@ package withbeetravel.service.settlement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import withbeetravel.domain.TravelMember;
+import withbeetravel.domain.TravelMemberSettlementHistory;
 import withbeetravel.dto.settlement.ShowMyTotalPaymentResponse;
+import withbeetravel.dto.settlement.ShowOtherSettlementResponse;
+import withbeetravel.dto.settlement.ShowSettlementDetailResponse;
 import withbeetravel.exception.CustomException;
+import withbeetravel.exception.error.SettlementErrorCode;
+import withbeetravel.exception.error.TravelErrorCode;
 import withbeetravel.repository.*;
 
-
-import static withbeetravel.exception.error.SettlementErrorCode.SETTLEMENT_NOT_FOUND;
-import static withbeetravel.exception.error.TravelErrorCode.TRAVEL_ACCESS_FORBIDDEN;
-import static withbeetravel.exception.error.TravelErrorCode.TRAVEL_NOT_FOUND;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SettlementServiceImpl implements SettlementService{
 
-    private final TravelRepository travelRepository;
     private final TravelMemberRepository travelMemberRepository;
     private final SettlementRequestRepository settlementRequestRepository;
+    private final TravelMemberSettlementHistoryRepository travelMemberSettlementHistoryRepository;
+    private final PaymentParticipatedMemberRepository paymentParticipatedMemberRepository;
 
     @Override
-    public ShowMyTotalPaymentResponse getMyTotalPayments(Long userId, Long travelId, Long settlementRequestId) {
-        travelRepository.findById(travelId).orElseThrow(() -> new CustomException(TRAVEL_NOT_FOUND));
-        TravelMember travelMember = travelMemberRepository.findByUserIdAndTravelId(userId, travelId).orElseThrow(() -> new CustomException(TRAVEL_ACCESS_FORBIDDEN));
-        settlementRequestRepository.findById(settlementRequestId).orElseThrow(() -> new CustomException(SETTLEMENT_NOT_FOUND));
+    public ShowSettlementDetailResponse getSettlementDetails(Long userId, Long travelId, Long settlementRequestId) {
+        validateSettlement(settlementRequestId);
+        Long myTravelMemberId = findMyTravelMemberIdByUserIdAndTravelId(userId, travelId);
+
+        List<TravelMemberSettlementHistory> travelMemberSettlementHistories =
+                travelMemberSettlementHistoryRepository.findAllBySettlementRequestId(settlementRequestId);
+
+        ShowMyTotalPaymentResponse myTotalPayments =
+                travelMemberSettlementHistories
+                        .stream()
+                        .filter(history -> history.getTravelMember().getId().equals(myTravelMemberId))
+                        .findFirst()
+                        .map(history -> {
+                            int ownPaymentCost = history.getOwnPaymentCost();
+                            int actualBurdenCost = history.getActualBurdenCost();
+                            int totalPaymentCost = ownPaymentCost - actualBurdenCost;
+
+                            return ShowMyTotalPaymentResponse.of(totalPaymentCost, ownPaymentCost, actualBurdenCost);
+                        })
+                        .orElseThrow(() -> new CustomException(SettlementErrorCode.MEMBER_SETTLEMENT_HISTORY_NOT_FOUND));
+
+        List<ShowOtherSettlementResponse> others =
+                travelMemberSettlementHistories
+                        .stream()
+                        .filter(history -> !history.getTravelMember().getId().equals(myTravelMemberId))
+                        .map(history -> {
+                            Long travelMemberId = history.getTravelMember().getId();
+                            TravelMember travelMember = travelMemberRepository.findById(travelMemberId).orElseThrow(() -> new CustomException(TravelErrorCode.TRAVEL_ACCESS_FORBIDDEN));
+                            String memberName = travelMember.getUser().getName();
+                            int totalPaymentCost = history.getOwnPaymentCost() - history.getActualBurdenCost();
+                            return ShowOtherSettlementResponse.of(memberName, totalPaymentCost);
+                        })
+                        .collect(Collectors.toList());
 
         return null;
+    }
+
+    private Long findMyTravelMemberIdByUserIdAndTravelId(Long userId, Long travelId) {
+        TravelMember userTravelMember =
+                travelMemberRepository.findByUserIdAndTravelId(userId, travelId).orElseThrow(() -> new CustomException(TravelErrorCode.TRAVEL_ACCESS_FORBIDDEN));
+        return userTravelMember.getId();
+    }
+
+    private void validateSettlement(Long settlementRequestId) {
+        settlementRequestRepository.findById(settlementRequestId).orElseThrow(() -> new CustomException(SettlementErrorCode.SETTLEMENT_NOT_FOUND));
     }
 }
