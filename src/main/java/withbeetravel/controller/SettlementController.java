ca9af8fd122dc5b43e411e266861c9f954b68717
yaejinkong <0910a@naver.com>
2024-11-15 01:58:43 +0900

    [feat/TK-128] controller 추가

---
+++ src/main/java/withbeetravel/controller/SettlementController.java
@@ -0,0 +1,22 @@
+package withbeetravel.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import withbeetravel.dto.settlement.ShowSettlementDetailResponse;
+import withbeetravel.service.settlement.SettlementService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/travels/{travelId}/settlements")
+public class SettlementController {
+    private final SettlementService settlementService;
+
+    @GetMapping("/{settlementId}")
+    ShowSettlementDetailResponse getSettlementDetails(@PathVariable Long travelId, @PathVariable Long settlementId) {
+        Long userId = 3L;
+        return settlementService.getSettlementDetails(userId, travelId, settlementId);
+    }
+}
