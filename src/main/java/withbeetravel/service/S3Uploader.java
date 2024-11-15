a5c42b6f61a0c6e34bc21e86fb21d860bc31bf5f
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-11-15 10:32:52 +0900

    [feat/TK-135]: s3 파일 형식 수정

--- src/main/java/withbeetravel/service/S3Uploader.java
+++ src/main/java/withbeetravel/service/S3Uploader.java
@@ -2,13 +2,14 @@ package withbeetravel.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.net.URLDecoder;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -26,6 +27,8 @@ public class S3Uploader {
     }
 
     // 이미지 저장
+    // file := s3에 저장할 이미지 파일
+    // dirName := 이미지 파일을 저장할 s3 디렉토리
     public String upload(MultipartFile file, String dirName) throws IOException {
 
         // 파일의 원래 이름에서 공백을 제거
@@ -38,17 +41,17 @@ public class S3Uploader {
         // 디렉토리 위치와 파일명 합치기
         String fileName = dirName + "/" + uniqueFileName;
 
-        // MultipartFile -> File
-//        File uploadFile =convert(file, uniqueFileName);
-
         // S3에 이미지 업로드
-        String uploadImageUrl = putS3(file.getInputStream(), fileName);
+        String contentType = file.getContentType();
+        if(contentType == null) contentType = "application/octet-stream";
+        String uploadImageUrl = putS3(file.getInputStream(), fileName, file.getSize(), contentType);
 
         // S3에 저장된 이미지의 URL 리턴
         return uploadImageUrl;
     }
 
     // 이미지 삭제
+    // filName := 삭제할 이미지명(URL 형식)
     public void delete(String fileName) {
         if(fileName.startsWith(bucketDomain)) {
             amazonS3.deleteObject(bucket, fileName.substring(bucketDomain.length()));
@@ -56,6 +59,9 @@ public class S3Uploader {
     }
 
     // 이미지 수정
+    // newFile := 새로 저장할 이미지 파일
+    // oldFileName := 기존에 저장되어 있던 이미지명(URL 형식)
+    // dirName := 이미지 파일을 저장할 s3 디렉토리
     public String update(MultipartFile newFile, String oldFileName, String dirName) throws IOException {
         // 기존 파일 삭제
         delete(oldFileName);
@@ -63,25 +69,13 @@ public class S3Uploader {
         return upload(newFile, dirName);
     }
 
-    // MultipartFile -> File
-    private File convert(MultipartFile file, String uniqueFileName) throws IOException {
-
-        File convertFile = new File(uniqueFileName);
-        if (convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            } catch (IOException e) {
-                System.err.println("파일 변환 중 오류 발생: " +  e.getMessage());
-                throw e;
-            }
-            return convertFile;
-        }
-        throw new IllegalArgumentException("파일 변환에 실패했습니다. " + file.getOriginalFilename());
-    }
-
     // S3에 이미지 업로드
-    private String putS3(InputStream inputStream, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, null)
+    private String putS3(InputStream inputStream, String fileName, long contentLength, String contentType) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(contentLength);
+        metadata.setContentType(contentType); // Content-Type 설정
+
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, metadata)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3.getUrl(bucket, fileName).toString();
     }
