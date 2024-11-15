c9f1eaeb757b14cc0609bb0582142611dcadebdc
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-11-15 10:17:22 +0900

    [feat/TK-135]: s3 업로드 형식 변경

--- src/main/java/withbeetravel/service/S3Uploader.java
+++ src/main/java/withbeetravel/service/S3Uploader.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URLDecoder;
 import java.util.UUID;
 
@@ -42,13 +39,10 @@ public class S3Uploader {
         String fileName = dirName + "/" + uniqueFileName;
 
         // MultipartFile -> File
-        File uploadFile =convert(file, uniqueFileName);
+//        File uploadFile =convert(file, uniqueFileName);
 
         // S3에 이미지 업로드
-        String uploadImageUrl = putS3(uploadFile, fileName);
-
-        // 임시 파일 삭제
-        uploadFile.delete();
+        String uploadImageUrl = putS3(file.getInputStream(), fileName);
 
         // S3에 저장된 이미지의 URL 리턴
         return uploadImageUrl;
@@ -86,8 +80,8 @@ public class S3Uploader {
     }
 
     // S3에 이미지 업로드
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+    private String putS3(InputStream inputStream, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, null)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3.getUrl(bucket, fileName).toString();
     }
