1b8f9e90b1fe7c1dbed6b43f95caf4081aa976a7
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-11-15 10:31:51 +0900

    [feat/TK-113]: s3 파일 형식 수정

--- src/main/java/withbeetravel/service/S3Uploader.java
+++ src/main/java/withbeetravel/service/S3Uploader.java
@@ -2,14 +2,14 @@ package withbeetravel.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -41,14 +41,10 @@ public class S3Uploader {
         // 디렉토리 위치와 파일명 합치기
         String fileName = dirName + "/" + uniqueFileName;
 
-        // MultipartFile -> File
-        File uploadFile =convert(file, uniqueFileName);
-
         // S3에 이미지 업로드
-        String uploadImageUrl = putS3(uploadFile, fileName);
-
-        // 임시 파일 삭제
-        uploadFile.delete();
+        String contentType = file.getContentType();
+        if(contentType == null) contentType = "application/octet-stream";
+        String uploadImageUrl = putS3(file.getInputStream(), fileName, file.getSize(), contentType);
 
         // S3에 저장된 이미지의 URL 리턴
         return uploadImageUrl;
@@ -73,25 +69,13 @@ public class S3Uploader {
         return upload(newFile, dirName);
     }
 
-    // MultipartFile -> File
-    private File convert(MultipartFile file, String uniqueFileName) throws IOException {
-
-        File convertFile = new File(uniqueFileName);
-        if (convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            } catch (IOException e) {
-                System.err.println("파일 변환 중 오류 발생: " +  e.getMessage());
-                throw e;
-            }
-            return convertFile;
-        }
-        throw new IllegalArgumentException("파일 변환에 실패했습니다. " + file.getOriginalFilename());
-    }
-
     // S3에 이미지 업로드
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+    private String putS3(InputStream inputStream, String fileName, long contentLength, String contentType) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(contentLength);
+        metadata.setContentType(contentType); // Content-Type 설정
+
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, metadata)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3.getUrl(bucket, fileName).toString();
     }
