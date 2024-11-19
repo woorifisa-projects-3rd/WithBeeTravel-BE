a203c8e22e7c45926ebc89a83fb892978f3979b2
HoChoRoo <rttkhc0304@gmail.com>
2024-11-19 20:38:37 +0900

    [fix/TK-156]: 테스트 코드 수정

--- src/test/java/withbeetravel/WithbeetravelApplicationTests.java
+++ src/test/java/withbeetravel/WithbeetravelApplicationTests.java
@@ -2,12 +2,8 @@ package withbeetravel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.properties")
 class WithbeetravelApplicationTests {
 
 	@Test
