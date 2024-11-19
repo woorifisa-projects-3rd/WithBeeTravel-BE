c8a68b333314f4056b09b9ba9707cb092f32632c
HoChoRoo <rttkhc0304@gmail.com>
2024-11-19 10:22:27 +0900

    [feat/TK-156]: BankingErrorCode에 위비카드 연결여부 확인 에러코드 추가

--- src/main/java/withbeetravel/exception/error/BankingErrorCode.java
+++ src/main/java/withbeetravel/exception/error/BankingErrorCode.java
@@ -15,6 +15,8 @@ public class BankingErrorCode extends ErrorCode{
     public static final BankingErrorCode TRANSFER_LIMIT_EXCEEDED
             = new BankingErrorCode(HttpStatus.BAD_REQUEST,"TRANSFER_LIMIT_EXCEEDED","BANKING-003","하루 이체 한도 초과");
 
+    public static final BankingErrorCode NOT_CONNECTED_WIBEE_ACCOUNT
+            = new BankingErrorCode(HttpStatus.BAD_REQUEST,"NOT_CONNECTED_WIBEE_ACCOUNT","BANKING-004","위비 카드가 연결되어있지 않습니다.");
 
 
     public BankingErrorCode(HttpStatus status,
