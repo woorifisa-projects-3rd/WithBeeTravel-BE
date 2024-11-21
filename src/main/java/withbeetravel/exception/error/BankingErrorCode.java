485f68c59ebc9090517e3eb8762e6c2c235cb819
HoChoRoo <rttkhc0304@gmail.com>
2024-11-21 14:51:33 +0900

    [hotfix]: 뱅킹에러코드 정상화

--- src/main/java/withbeetravel/exception/error/BankingErrorCode.java
+++ src/main/java/withbeetravel/exception/error/BankingErrorCode.java
@@ -27,6 +27,9 @@ public class BankingErrorCode extends ErrorCode{
     public static final BankingErrorCode PAYMENT_ALREADY_EXISTS
             = new BankingErrorCode(HttpStatus.NOT_FOUND,"PAYMENT_ALREADY_EXISTS","BANKING-007","이미 추가된 결제 내역입니다");
 
+    public static final BankingErrorCode INVALID_PIN_NUMBER
+            = new BankingErrorCode(HttpStatus.BAD_REQUEST, "INVALID_PIN_NUMBER", "BANKING-009", "잘못된 핀 번호입니다");
+
     public BankingErrorCode(HttpStatus status,
                             String name, String code, String message) {
         super(status, name, code, message);
