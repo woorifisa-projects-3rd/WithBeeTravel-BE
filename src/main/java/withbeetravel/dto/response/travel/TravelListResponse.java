2fdd3ab5afecddb90d161a845b166e208d252ed2
LEEDOYI <doyi9917@naver.com>
2024-11-23 10:59:33 +0900

    [fix/TK-176]:mainImage타입 수정

--- src/main/java/withbeetravel/dto/response/travel/TravelListResponse.java
+++ src/main/java/withbeetravel/dto/response/travel/TravelListResponse.java
@@ -15,6 +15,6 @@ public class TravelListResponse {
     private String travelName;
     private String travelStartDate;
     private String travelEndDate;
-    private String travelMainImage;
+    private int travelMainImage;
     private int profileImage;
 }
