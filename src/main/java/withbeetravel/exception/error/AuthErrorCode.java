c0649fbe2d8a35bf73e2924d7270555ce300f99f
yaejinkong <0910a@naver.com>
2024-11-24 16:00:34 +0900

    [refactor/TK-187] JwtAuthFilter에서 유효하지 않은 Token 시 커스텀 에러와 동일하게 에러 전송하도록 jwtExceptionHandler 메서드 추가

--- src/main/java/withbeetravel/exception/error/AuthErrorCode.java
+++ src/main/java/withbeetravel/exception/error/AuthErrorCode.java
@@ -16,6 +16,8 @@ public class AuthErrorCode extends ErrorCode{
     public static final AuthErrorCode INVALID_EMAIL_FORMAT = new AuthErrorCode(HttpStatus.BAD_REQUEST, "AUTH-008", "INVALID_EMAIL_FORMAT", "잘못된 이메일 형식");
     public static final AuthErrorCode PIN_POLICY_VIOLATION = new AuthErrorCode(HttpStatus.BAD_REQUEST, "PIN_POLICY_VIOLATION", "AUTH-009", "핀번호 정책 불충족");
     public static final AuthErrorCode INVALID_PASSWORD = new AuthErrorCode(HttpStatus.BAD_REQUEST, "INVALID_PASSWORD", "AUTH-010", "잘못된 비밀번호");
+    public static final AuthErrorCode INVALID_JWT = new AuthErrorCode(HttpStatus.UNAUTHORIZED, "INVALID_JWT", "AUTH-011", "잘못된 JWT 토큰");
+    public static final AuthErrorCode EXPIRED_JWT = new AuthErrorCode(HttpStatus.UNAUTHORIZED, "EXPIRED_JWT", "AUTH-012", "만료된 JWT 토큰");
 
     private AuthErrorCode(HttpStatus status, String name, String code, String message) {
         super(status, name, code, message);
--- src/main/java/withbeetravel/jwt/JwtAuthFilter.java
+++ src/main/java/withbeetravel/jwt/JwtAuthFilter.java
@@ -1,14 +1,18 @@
 package withbeetravel.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
+import withbeetravel.dto.response.ErrorResponse;
+import withbeetravel.exception.error.AuthErrorCode;
 import withbeetravel.service.auth.CustomUserDetailsService;
 
 import java.io.IOException;
@@ -44,8 +48,36 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext()
                             .setAuthentication(usernamePasswordAuthenticationToken);
                 }
+            } else {
+                if (!jwtUtil.isValidToken(token)) {
+                    jwtExceptionHandler(response, AuthErrorCode.EXPIRED_JWT);
+                    return; // 필터 체인 중단
+                } else {
+                    jwtExceptionHandler(response, AuthErrorCode.INVALID_JWT);
+                    return;
+                }
             }
         }
         filterChain.doFilter(request, response); // 다음 필터로 넘김
+    }
+
+    public void jwtExceptionHandler(HttpServletResponse response, AuthErrorCode authErrorCode) {
+        try {
+            // ErrorResponse 생성
+            ResponseEntity<ErrorResponse> errorResponseEntity = ErrorResponse.toResponseEntity(authErrorCode);
+
+            // Http 상태 코드 설정
+            response.setStatus(errorResponseEntity.getStatusCodeValue());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            // JSON으로 변환하여 응답 본문에 작성
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResponse = objectMapper.writeValueAsString(errorResponseEntity.getBody());
+            response.getWriter().write(jsonResponse);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
