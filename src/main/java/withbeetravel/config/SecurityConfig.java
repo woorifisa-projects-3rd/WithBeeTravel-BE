cec1f5d36be47da3479035b4830887bf74f54319
yaejinkong <0910a@naver.com>
2024-11-24 12:26:49 +0900

    [feat/TK-187] Config 추가

--- src/main/java/withbeetravel/config/SecurityConfig.java
+++ src/main/java/withbeetravel/config/SecurityConfig.java
@@ -1,23 +1,51 @@
 package withbeetravel.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import withbeetravel.jwt.JwtAuthFilter;
+import withbeetravel.jwt.JwtUtil;
+import withbeetravel.service.auth.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig{
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+    private static final String[] AUTH_WHITELIST = {"/api/auth/login", "/api/auth/join", "/swagger-ui/**", "/"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // 로그인 기능 구현 전까지 모든 요청 허용
-        http
-                .csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/**").permitAll());
+        // CSRF, CORS
+        http.csrf((csrf) -> csrf.disable());
+        http.cors((Customizer.withDefaults()));
+
+        // 세션 관리 상태 없음으로 구성, Spring Securtiy가 세션 생성 or 사용 x
+        http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
+
+        // FormLogin, BasicHttp 비활성화
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        // JwtAuthFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
+        http.addFilterBefore(new JwtAuthFilter(customUserDetailsService, jwtUtil),
+                UsernamePasswordAuthenticationFilter.class);
+
+        // 권한 규칙 작성
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated());
 
         return http.build();
     }
