a55ac489c5352dc90fa1297f17f2085d98f6a37c
yaejinkong <0910a@naver.com>
2024-11-25 14:54:38 +0900

    [refactor/TK-187] MethodArgumentNotValidException 에러 예외 핸들링 추가

--- src/main/java/withbeetravel/exception/error/AuthErrorCode.java
+++ src/main/java/withbeetravel/exception/error/AuthErrorCode.java
@@ -22,6 +22,7 @@ public class AuthErrorCode extends ErrorCode{
     public static final AuthErrorCode EMPTY_JWT = new AuthErrorCode(HttpStatus.BAD_REQUEST, "EMPTY_JWT", "AUTH-014", "빈 JWT 토큰");
     public static final AuthErrorCode REFRESH_TOKEN_NOT_FOUND = new AuthErrorCode(HttpStatus.NOT_FOUND, "REFRESH_TOKEN_NOT_FOUND", "AUTH-015", "리프레시 토큰 없음");
     public static final AuthErrorCode INVALID_REFRESH_TOKEN = new AuthErrorCode(HttpStatus.UNAUTHORIZED, "INVALID_REFRESH_TOKEN", "AUTH-016", "잘못된 리프레시 토큰");
+    public static final AuthErrorCode VALIDATION_FAILED = new AuthErrorCode(HttpStatus.BAD_REQUEST, "VALIDATION_FAILED", "AUTH-017", "유효성 검사 실패");
 
 
     private AuthErrorCode(HttpStatus status, String name, String code, String message) {
--- src/main/java/withbeetravel/exception/handler/CustomExceptionHandler.java
+++ src/main/java/withbeetravel/exception/handler/CustomExceptionHandler.java
@@ -1,10 +1,13 @@
 package withbeetravel.exception.handler;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import withbeetravel.exception.CustomException;
 import withbeetravel.dto.response.ErrorResponse;
+import withbeetravel.exception.error.AuthErrorCode;
+
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -12,5 +15,24 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleAuthException(CustomException e) {
         return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        // 유효성 검사에서 실패한 필드와 오류 메시지를 추출
+        String errorMessages = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .reduce((msg1, msg2) -> msg1 + ", " + msg2)
+                .orElse("유효성 검사 실패");
+
+        // ErrorResponse 객체를 생성하여 응답 반환
+        AuthErrorCode validationFailed = AuthErrorCode.VALIDATION_FAILED;
+        return ResponseEntity.status(validationFailed.getStatus())
+                .body(ErrorResponse.builder()
+                        .status(validationFailed.getStatus().value())
+                        .name(validationFailed.getName())
+                        .code(validationFailed.getCode())
+                        .message(errorMessages)
+                        .build());
     }
 }
