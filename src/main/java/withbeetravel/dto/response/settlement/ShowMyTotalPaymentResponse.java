d40dfbdc125c198a3c6d25c22e4b805656715385
yaejinkong <0910a@naver.com>
2024-11-27 06:01:58 +0900

    [refactor/TK-187]: 상세 내역 조회 API 수정
    - 내가 결제했지만 정산 인원에 포함되지 않는 경우를 고려하도록 수정

--- src/main/java/withbeetravel/dto/response/settlement/ShowMyTotalPaymentResponse.java
+++ src/main/java/withbeetravel/dto/response/settlement/ShowMyTotalPaymentResponse.java
@@ -4,21 +4,17 @@ import lombok.Getter;
 
 @Getter
 public class ShowMyTotalPaymentResponse {
-    private String name;
-    private boolean isAgreed;
-    private int totalPaymentCost;
-    private int ownPaymentCost;
-    private int actualBurdenCost;
+    private final String name;
+    private final boolean isAgreed;
+    private final int totalPaymentCost;
 
-    private ShowMyTotalPaymentResponse(String name, boolean isAgreed, int totalPaymentCost, int ownPaymentCost, int actualBurdenCost) {
+    private ShowMyTotalPaymentResponse(String name, boolean isAgreed, int totalPaymentCost) {
         this.name = name;
         this.isAgreed = isAgreed;
         this.totalPaymentCost = totalPaymentCost;
-        this.ownPaymentCost = ownPaymentCost;
-        this.actualBurdenCost = actualBurdenCost;
     }
 
     public static ShowMyTotalPaymentResponse of (String name, boolean isAgreed, int ownPaymentCost, int actualBurdenCost) {
-        return new ShowMyTotalPaymentResponse(name, isAgreed, ownPaymentCost - actualBurdenCost, ownPaymentCost, actualBurdenCost);
+        return new ShowMyTotalPaymentResponse(name, isAgreed, ownPaymentCost - actualBurdenCost);
     }
 }
