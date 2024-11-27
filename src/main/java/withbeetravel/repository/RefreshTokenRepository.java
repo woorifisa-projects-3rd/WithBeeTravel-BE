8d35600f7eb8c307ba74b49aa8d0970ea4128b99
yaejinkong <0910a@naver.com>
2024-11-28 06:04:19 +0900

    [refactor/TK-215]: 토큰 재발급 로직 변경
    - RefreshToken 엔티티와 레포지토리를 사용하도록 변경

--- src/main/java/withbeetravel/repository/RefreshTokenRepository.java
+++ src/main/java/withbeetravel/repository/RefreshTokenRepository.java
@@ -3,9 +3,12 @@ package withbeetravel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import withbeetravel.domain.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     boolean existsByUserId(Long userId);
     void deleteByUserId(Long userId);
 
+    Optional<RefreshToken> findByToken(String token);
 }
