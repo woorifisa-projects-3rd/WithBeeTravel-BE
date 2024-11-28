5811b1ad4a514fe0489070141f3fad54a5efe631
Kong-E <kng001016@dgu.ac.kr>
2024-11-28 11:44:33 +0900

    [feat/TK-183]: 여행 홈 API 필드 추가

--- src/main/java/withbeetravel/domain/TravelMember.java
+++ src/main/java/withbeetravel/domain/TravelMember.java
@@ -25,6 +25,9 @@ public class TravelMember {
     @Column(name = "is_captain", nullable = false)
     private boolean isCaptain;
 
+    @OneToOne(mappedBy = "travelMember")
+    private TravelMemberSettlementHistory settlementHistory;
+
     protected TravelMember() {}
 
     @Builder
