94954bcaa53b6c60b38bf51018ae5de10354e8a2
HoChoRoo <rttkhc0304@gmail.com>
2024-12-04 16:48:16 +0900

    [feat/TK-224]: 로그인 시도 횟수 조회 기능 추가

--- src/main/java/withbeetravel/repository/LoginLogRepository.java
+++ src/main/java/withbeetravel/repository/LoginLogRepository.java
@@ -3,8 +3,19 @@ package withbeetravel.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import withbeetravel.domain.LoginLog;
+import withbeetravel.domain.LoginLogType;
+
+import java.util.List;
 
 public interface LoginLogRepository extends JpaRepository<LoginLog,Long> {
     Page<LoginLog> findAllByUser_Id(Long id, Pageable pageable);
+
+    Page<LoginLog> findAllByUser_IdAndLoginLogType(Long userId, LoginLogType loginLogType, Pageable pageable);
+
+    @Query("SELECT COUNT(l) FROM LoginLog l WHERE l.loginLogType IN :types")
+    long countByLoginLogTypeIn(@Param("types") List<LoginLogType> loginLogTypes);
+
 }
