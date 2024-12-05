b9a7ad800038e89c412f9fae750c90d39ceab5b3
HoChoRoo <rttkhc0304@gmail.com>
2024-12-05 17:40:22 +0900

    [feat/TK-224]:S3Config 설정 변경(리전도 제외)

--- src/main/java/withbeetravel/config/S3Config.java
+++ src/main/java/withbeetravel/config/S3Config.java
@@ -12,14 +12,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 설정 파일을 읽기 위한 annotation
 public class S3Config {
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
 
     @Bean
     public AmazonS3 s3Client() {
 
         return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
                 .build();
     }
 }
