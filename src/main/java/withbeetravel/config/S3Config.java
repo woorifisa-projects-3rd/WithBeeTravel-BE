84391d3d6726befb11db499138a605d5a828b362
HoChoRoo <rttkhc0304@gmail.com>
2024-12-05 17:37:32 +0900

    [feat/TK-224]:S3Config 설정 변경

--- src/main/java/withbeetravel/config/S3Config.java
+++ src/main/java/withbeetravel/config/S3Config.java
@@ -12,22 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 설정 파일을 읽기 위한 annotation
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
     public AmazonS3 s3Client() {
 
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
                 .build();
     }
