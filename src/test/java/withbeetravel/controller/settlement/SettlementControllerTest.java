3f3330bd21d806533297bd88e244ad6485130409
yaejinkong <0910a@naver.com>
2024-12-07 06:50:20 +0900

    [feat/TK-253] 정산 요청, 동의, 취소 컨트롤러 테스트 코드 작성

---
+++ src/test/java/withbeetravel/controller/settlement/SettlementControllerTest.java
@@ -0,0 +1,51 @@
+package withbeetravel.controller.settlement;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import withbeetravel.controller.BaseIntegrationTest;
+
+import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
+
+class SettlementControllerTest extends BaseIntegrationTest {
+
+    @Test
+    void 정산을_요청한다() {
+        given().log().all()
+                .header(AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/travels/1/settlements")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 정산을_동의한다() {
+        given().log().all()
+                .header(AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/travels/1/settlements/agreement")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 정산을_취소한다() {
+        given().log().all()
+                .header(AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/api/travels/1/settlements")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
